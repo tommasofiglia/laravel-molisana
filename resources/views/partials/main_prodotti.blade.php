c39e06d2ecfbb4bdf87acb19387908d7bb1833f9
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-12 19:14:22 +0100

    Esercizio completato

--- resources/views/partials/main_prodotti.blade.php
+++ resources/views/partials/main_prodotti.blade.php
@@ -3,15 +3,22 @@
   @foreach ($paste as $pasta => $dettagli)
     <h2>{{$pasta}}</h1>
     <div class="card_container d-flex flex-wrap">
-    @foreach ($dettagli as $dettaglio)
 
+    @foreach ($dettagli as $dettaglio)
           <div class="card">
             <a href="#">
-              <img src="{{$dettaglio['src']}}" alt="">
+              <img src="{{$dettaglio['src']}}" alt="" class="pasta_img">
+              <div class="hover_card">
+                <div class="details_container">
+                  <img src="{{asset('img/icon.svg')}}" alt="" class="icon_forks">
+                  <h2>{{$dettaglio['titolo']}}</h2>
+                  <h4>Tempo di cottura: {{$dettaglio['cottura']}}</h4>   
+                </div>
+              </div>
             </a>
           </div>
-
     @endforeach
+
   </div>
   @endforeach
 
